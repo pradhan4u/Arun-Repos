278a61f0660624f0ef12fe27e9ffa5054f241959
pradhan4u <pradhan4u@gmail.com>
2016-09-21 12:12:21 +1000

    test

---
+++ aura/initComponent/initComponentController.js
@@ -0,0 +1,7 @@
+({
+    doInit: function(cmp) {
+        // Set the attribute value. 
+        // You could also fire an event here instead.
+        cmp.set("v.setMeOnInit", "controller init magic!");
+    }
+})
